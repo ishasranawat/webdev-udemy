002a0a39ffe1df900e9382ed82ae45e5e75fa26b
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-14 12:29:48 +0530

    Create JS9.js

---
+++ Assignments/JS9.js
@@ -0,0 +1,5 @@
+let button=document.createElement("button");
+let input=document.createElement("input");
+button.innerText="Clickme";
+document.querySelector("body").append(input);
+document.querySelector("body").append(button);
