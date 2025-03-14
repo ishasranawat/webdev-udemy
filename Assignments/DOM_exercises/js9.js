882dc46ec247f846ae274ff4f335017f7cc4ba8c
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-14 12:32:54 +0530

    Update and rename Assignments/JS9.js to Assignments/DOM_exercises/js9.js

--- Assignments/DOM_exercises/js9.js
+++ Assignments/DOM_exercises/js9.js
@@ -1,3 +1,4 @@
+//1.Create a new input and button element on the page using JavaScript only. Set the text of button to “Clickme”;
 let button=document.createElement("button");
 let input=document.createElement("input");
 button.innerText="Clickme";
