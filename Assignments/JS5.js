cda551b58b20ab2833e7e2c2e9c0c0b38b3215d9
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-17 16:55:58 +0530

    Update JS5.js

--- Assignments/JS5.js
+++ Assignments/JS5.js
@@ -1,3 +1,11 @@
 //Create a program that generates a random number representing a dice roll. [The number should be between 1 and 6]
 let dice= (Math.floor(Math.random()*6 +1));
 console.log(dice);
+
+
+Qs2.Createanobjectrepresentingacarthatstoresthefollowingpropertiesforthecar:name,model,color.Printthecar’sname.
+const car= {
+  name: "porsche",
+  model: "911",
+  color: "black"
+}
