728ecc445326e180aea92754637485273771c881
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-17 17:10:19 +0530

    Update JS5.js

--- Assignments/JS5.js
+++ Assignments/JS5.js
@@ -3,13 +3,15 @@ let dice= (Math.floor(Math.random()*6 +1));
 console.log(dice);
 
 
-Qs2.Createanobjectrepresentingacarthatstoresthefollowingpropertiesforthecar:name,model,color.Printthecar’sname.
+//Qs2.Createanobjectrepresentingacarthatstoresthefollowingpropertiesforthecar:name,model,color.Printthecar’sname.
 const car= {
   name: "porsche",
   model: "911",
   color: "black"
 }
 
+
+//Q3. CreateanobjectPersonwiththeirname,ageandcity.Edittheircity’soriginalvaluetochangeitto“NewYork”.AddanewpropertycountryandsetittotheUnitedState
 const person= {
   name: "Isha",
   age: "22",
