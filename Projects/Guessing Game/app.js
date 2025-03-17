04dda80e8ecfddc81654b5d6e123102a734f8570
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-17 17:27:57 +0530

    Update app.js

--- Projects/Guessing Game/app.js
+++ Projects/Guessing Game/app.js
@@ -2,19 +2,19 @@ let max= prompt("enter the maximum number");
 
 let random= Math.floor(Math.random()* max+1);
 
-let guess= prompt("guess the number");
+let guess= prompt("Guess the number");
 
 while(true){
   if(guess=="quit"){
-    console.log("the user quit the game.");
+    console.log("You quit the game.");
     break;
   }
 
   if (random==guess){
-  console.log("you won");
+  console.log("Congrats! You won");
   break;
 }
   else{
-  guess= prompt("try again")
+  guess= prompt("Your guess was incorrect, try again.")
 }
 }
