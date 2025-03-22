a9e084865b748f1eb2a6282109a75745367bc907
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-22 10:02:46 +0530

    Update Node.js

--- Javascript/Node.js
+++ Javascript/Node.js
@@ -1,11 +1,8 @@
 const fs= require("fs");
-
 fs.writeFile("message.txt", "HEllo from NodeJS!", (err) => {
   if (err) throw err;
 console.log("The file has been saved!")
 });
-
-
 fs.readFile("message.txt","utf8", (err, data) => {
   if (err) throw err;
   console.log(data);
