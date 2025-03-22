ed3f2949b8e7f852604c56e58d51b9abbf84b71e
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-22 10:03:38 +0530

    Update Node.js

--- Javascript/Node.js
+++ Javascript/Node.js
@@ -1,8 +1,11 @@
+//To write in a file
 const fs= require("fs");
 fs.writeFile("message.txt", "HEllo from NodeJS!", (err) => {
   if (err) throw err;
 console.log("The file has been saved!")
 });
+
+//To read a file (use utf8 else it will read in binary)
 fs.readFile("message.txt","utf8", (err, data) => {
   if (err) throw err;
   console.log(data);
