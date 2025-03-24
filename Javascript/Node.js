2736f0b49e10a8e6ab2feb775abc845df77fbf48
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-24 23:05:40 +0530

    Update Node.js

--- Javascript/Node.js
+++ Javascript/Node.js
@@ -10,8 +10,7 @@ console.log("The file has been saved!")
 fs.readFile("message.txt","utf8", (err, data) => {
   if (err) throw err;
   console.log(data);
-}); 
-
+});
 
 /------------------npm package-----------------------------/
 var generateName= require ("sillyname");  //for CJS (the "type" : "commonjs" in JSON file)
