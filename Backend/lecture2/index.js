c34d6450d0b3f8d82c0ac3e2ac0afa074ad4dcf9
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-28 00:41:09 +0530

    Update index.js

--- Backend/lecture2/index.js
+++ Backend/lecture2/index.js
@@ -1,1 +1,47 @@
+import express from 'express';
+const app= express();
 
+// app.get('/', (req, res)=>{
+//   res.send("Server is ready");
+// })
+
+app.get('/jokes', (req, res)=>{
+  const jokes =[
+    {
+      id: 1,
+      title:'joke1',
+      joke:'haha'
+    },
+
+    {
+      id: 2,
+      title:'joke2',
+      joke:'hahaa'
+    },
+
+    {
+      id: 3,
+      title:'joke3',
+      joke:'hahaaa'
+    },
+
+    {
+      id: 4,
+      title:'joke4',
+      joke:'hahaaaa'
+    },
+
+    {
+      id: 5,
+      title:'joke5',
+      joke:'hahaaaaa'
+    }
+  ]
+  res.send(jokes);
+})
+
+const port=process.env.PORT ||3000;
+
+app.listen(port, ()=>{
+  console.log(`Server is running at port ${port}`);
+})
