130a50d432c5449a25c95ccf27f9b9b960e91c46
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-04-02 20:49:15 +0530

    Create two.js

---
+++ React/two.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Fruits</h1>
+    <ul>
+      <li>Watermelon</li>
+      <li>Kiwi</li>
+      <li>Pineapple</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
